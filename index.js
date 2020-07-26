a147aa82f37010edf664816f1215e4e24d850d2b
PLWpl <kontakt@pwisniewski.pl>
2020-07-26 22:24:30 +0200

    veeery minor changes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const chalk = require("chalk");
 const lineByLine = require("n-readlines");
 
-getSvgFile = (fileName) => {
-  const liner = new lineByLine(fileName);
+getSvgFile = (inputFileName, outputFileName = 'icon') => {
+  const liner = new lineByLine(inputFileName);
   let line;
   let lineNumber = 1;
 
@@ -16,13 +16,19 @@ getSvgFile = (fileName) => {
   while ((line = liner.next())) {
     let currentLineNumber = lineNumber++;
     let currentLineContent = line.toString('utf8');
-    const fileName = "icon" + currentLineNumber + '.svg';
+    const fileName = outputFileName + currentLineNumber + '.svg';
 
     fs.appendFile(fileName, currentLineContent, (err) => {
       if (err) throw err;
       console.log(`${fileName} created!`);
     })
   }
+
+  console.log(
+    chalk.blue(
+      `-------------------------------------------------Finished`
+    )
+  );
 };
 
 // each line in the file passed bellow needs to be a separate svg, starting with <svg> and ending with </svg>
